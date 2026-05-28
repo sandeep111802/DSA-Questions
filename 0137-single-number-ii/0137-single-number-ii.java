d4fc1a04e180c87da712a5471105df4b7aac73d8
sandeep111802 <sandeepsharma111802@gmail.com>
2026-05-29 02:16:37 +0530

    Time: 4 ms (58.55%), Space: 45.3 MB (61.15%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+
+        int result = 0;
+
+        for (int k = 0; k < 32; k++) {
+            int temp = (1 << k);
+
+            int countZeroes = 0;
+            int countOnes = 0;
+
+            for (int num : nums) {
+                if ((num & temp) == 0) {
+                    countZeroes++;
+                } else {
+                    countOnes++;
+                }
+            }
+
+            if (countOnes % 3 == 1) {
+                result = result | temp;
+            }
+        }
+
+        return result;
+    }
+}
