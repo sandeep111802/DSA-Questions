eb1b9b477b7639431563c591100ec54a86326d8e
sandeep111802 <sandeepsharma111802@gmail.com>
2026-05-31 11:49:33 +0530

    Time: 2 ms (100%), Space: 137.8 MB (43.88%) - LeetHub

---
+++ 1734-decode-xored-permutation/1734-decode-xored-permutation.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] decode(int[] encoded) {
+        int first = 0;
+        int n = encoded.length + 1;
+
+        for (int i = 1; i <= n; i++) {
+            first ^= i;
+        }
+
+        for (int i = 1; i < n - 1; i += 2) {
+            first ^= encoded[i];
+        }
+
+        int[] perm = new int[n];
+        perm[0] = first;
+
+        for (int i = 1; i < n; i++) {
+            perm[i] = perm[i - 1] ^ encoded[i - 1];
+        }
+
+        return perm;
+    }
+}
