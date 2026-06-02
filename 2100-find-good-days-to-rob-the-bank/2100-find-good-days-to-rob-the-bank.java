7efac897e5d43bdbea08c5f26a036c2248b672cd
sandeep111802 <sandeepsharma111802@gmail.com>
2026-06-02 18:43:09 +0530

    Time: 9 ms (15.9%), Space: 98.8 MB (65.02%) - LeetHub

---
+++ 2100-find-good-days-to-rob-the-bank/2100-find-good-days-to-rob-the-bank.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<Integer> goodDaysToRobBank(int[] security, int time) {
+         int n = security.length;
+
+        int[] left = new int[n];
+        int[] right = new int[n];
+
+        for (int i = 1; i < n; i++) {
+            if (security[i] <= security[i - 1]) {
+                left[i] = left[i - 1] + 1;
+            }
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            if (security[i] <= security[i + 1]) {
+                right[i] = right[i + 1] + 1;
+            }
+        }
+
+        List<Integer> ans = new ArrayList<>();
+
+        for (int i = time; i < n - time; i++) {
+            if (left[i] >= time && right[i] >= time) {
+                ans.add(i);
+            }
+        }
+
+        return ans;
+    }
+}
