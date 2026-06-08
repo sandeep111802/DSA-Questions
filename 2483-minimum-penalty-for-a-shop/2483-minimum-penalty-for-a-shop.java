6f473b491ad07979d15f6ef31cda1f977d032b96
sandeep111802 <sandeepsharma111802@gmail.com>
2026-06-09 04:29:03 +0530

    Time: 14 ms (27.07%), Space: 46.9 MB (57.51%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.java
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.java
@@ -1,30 +1,29 @@
 class Solution {
     public int bestClosingTime(String customers) {
-          int penalty = 0;
+        int n = customers.length();
+        int[] prefixN = new int[n + 1];
+        int[] suffixY = new int[n + 1];
 
-        for (char c : customers.toCharArray()) {
-            if (c == 'Y') {
-                penalty++;
-            }
+        for (int i = 1; i <= n; i++) {
+            prefixN[i] = prefixN[i - 1];
+            if (customers.charAt(i - 1) == 'N')
+                prefixN[i]++;
         }
 
-        int minPenalty = penalty;
-        int minHour = 0;
-
-        for (int i = 0; i < customers.length(); i++) {
-
-            if (customers.charAt(i) == 'Y') {
-                penalty--;
-            } else {
-                penalty++;
-            }
-
-            if (penalty < minPenalty) {
-                minPenalty = penalty;
-                minHour = i + 1;
-            }
+        for (int i = n - 1; i >= 0; i--) {
+            suffixY[i] = suffixY[i + 1];
+            if (customers.charAt(i) == 'Y')
+                suffixY[i]++;
         }
 
-        return minHour;
+        int best = Integer.MAX_VALUE, ans = 0;
+        for (int j = 0; j <= n; j++) {
+            int pen = prefixN[j] + suffixY[j];
+            if (pen < best) {
+                best = pen;
+                ans = j;
+            }
+        }
+        return ans;
     }
 }
